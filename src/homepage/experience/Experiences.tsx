8c8ad2183ae02a6b60299d604a11a794e4f80c0a
ramesh <rameshbaswapuram1@gmail.com>
2026-02-02 15:35:19 +0530

    resume updated

--- src/homepage/experience/Experiences.tsx
+++ src/homepage/experience/Experiences.tsx
@@ -16,10 +16,24 @@ interface Experience {
 }
 
 const experiencesData: Experience[] = [
-  { id: 1, title: "Lead Engineer", company: "HCLTech", duration: "(Feb 2025 - Present)" },
-  { id: 2, title: "Junior Software Engineer", company: "Extended Web AppTech LLP", duration: "(Oct 2023 - Jan 2025)" },
-  { id: 3, title: "FullStack Developer", company: "Gincat Software Solutions LLP", duration: "(Aug 2022 - Sep 2023)" },
-  { id: 4, title: "Content Management Advisor", company: "Data Core Technologies Limited", duration: "(Dec 2021 - Aug 2022)" }
+  {
+    id: 1,
+    title: "Lead Engineer",
+    company: "HCLTech",
+    duration: "(Feb 2025 - Present)",
+  },
+  {
+    id: 2,
+    title: "Junior Software Engineer",
+    company: "Extended Web AppTech LLP",
+    duration: "(Oct 2023 - Jan 2025)",
+  },
+  {
+    id: 3,
+    title: "Content Management Advisor",
+    company: "Data Core Technologies Limited",
+    duration: "(Dec 2021 - Aug 2022)",
+  },
 ];
 
 const Experiences: React.FC = () => {
@@ -28,7 +42,7 @@ const Experiences: React.FC = () => {
     ...exp,
     title: t(`experience.positions.${i}.title`),
     company: t(`experience.positions.${i}.company`),
-    duration: t(`experience.positions.${i}.duration`)
+    duration: t(`experience.positions.${i}.duration`),
   }));
 
   return (
@@ -43,15 +57,29 @@ const Experiences: React.FC = () => {
       <Box sx={styles.rightSection}>
         <Stack spacing={2}>
           {experiences.map((experience) => (
-            <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
+            <GlowCard
+              key={experience.id}
+              identifier={`experience-${experience.id}`}
+            >
               <Box sx={styles.glowCardBox}>
-                <Box component="img" src={Blur23} alt="Hero" sx={styles.blurBg} />
+                <Box
+                  component="img"
+                  src={Blur23}
+                  alt="Hero"
+                  sx={styles.blurBg}
+                />
                 <Box sx={styles.durationBox}>
                   <Typography variant="body2" sx={styles.durationText}>
                     {experience.duration}
                   </Typography>
                 </Box>
-                <Stack direction="row" alignItems="center" spacing={3} px={3} py={3}>
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  spacing={3}
+                  px={3}
+                  py={3}
+                >
                   <Box sx={styles.icon}>
                     <BsPersonWorkspace size={36} />
                   </Box>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,6 @@ export const links = {
   github: "https://github.com/yourprofile",
   linkedIn: "https://linkedin.com/in/yourprofile",
   stackOverflow: "https://stackoverflow.com/users/24862926/ramesh-goud",
-  resumeDrive:"https://drive.google.com/file/d/1IteReyDQcxoe-bNbmJoohLErYivLh6gX/view?usp=drive_link"
-              
+  resumeDrive:
+    "https://drive.google.com/file/d/13wO_VnroQj68uRlcGGLzrCwKB8RX7moU/view?usp=drive_link",
 };
